fa7fe4ddd1391c961cf06b0249160cc72cc259c7
Matheus-D <59287764+Matheus-D@users.noreply.github.com>
2023-06-02 17:45:13 -0400

    03 - Filter - Aplicando diferentes filtros

--- alura_books-/app/main.js
+++ alura_books-/app/main.js
@@ -5,7 +5,6 @@ getBuscarLivrosDaAPI()
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
-    let livrosComDesconto = aplicarDesconto(livros)
-    exibirOsLivrosNaTela(livrosComDesconto);
+    livros = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
-
